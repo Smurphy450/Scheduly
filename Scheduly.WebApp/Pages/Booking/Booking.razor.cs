bccfcbcc3db55b0fe4e60e4ee4ce5c381821b7b1
Kevin <kevinhornbek@gmail.com>
2024-11-14 16:33:00 +0100

    Added more pages to NavMenu

---
+++ Scheduly.WebApp/Pages/Booking/Booking.razor.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Scheduly.WebApp.Pages.Booking
+{
+	public class BookingBase : ComponentBase
+	{
+	}
+}
---
+++ Scheduly.WebApp/Pages/Overview/Overview.razor.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Scheduly.WebApp.Pages.Overview
+{
+	public class OverviewBase : ComponentBase
+	{
+	}
+}
