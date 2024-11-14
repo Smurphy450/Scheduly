dacda8d73fff776c8a26c13fc610a19402c1fc7c
Kevin <kevinhornbek@gmail.com>
2024-11-14 16:21:07 +0100

    NavMenu WIP

---
+++ Scheduly.WebApp/Pages/Profile/Profile.razor.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Scheduly.WebApp.Pages.Profile
+{
+    public class ProfileBase : ComponentBase
+    {
+    }
+}
--- Scheduly.WebApp/Program.cs
+++ Scheduly.WebApp/Program.cs
@@ -1,22 +1,25 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
-using Scheduly.WebApp.Pages.Shared;
+using MudBlazor;
+using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<Drawer>();
+
+// Add MudBlazor services
+builder.Services.AddMudServices();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+	app.UseExceptionHandler("/Error");
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
 }
 
 app.UseHttpsRedirection();
