de2bb36226299e9103885d67856331015bf5d3e1
Kevin <kevinhornbek@gmail.com>
2024-11-14 11:08:46 +0100

    Added a controller

---
+++ Scheduly.WebApi/Controllers/AbsenceController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Scheduly.WebApi.Models;
+
+namespace Scheduly.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AbsenceController : ControllerBase
+    {
+        private readonly SchedulyContext _context;
+
+        public AbsenceController(SchedulyContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Absence
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Absence>>> GetAbsences()
+        {
+            return await _context.Absences.ToListAsync();
+        }
+
+        // GET: api/Absence/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Absence>> GetAbsence(int id)
+        {
+            var absence = await _context.Absences.FindAsync(id);
+
+            if (absence == null)
+            {
+                return NotFound();
+            }
+
+            return absence;
+        }
+
+        // PUT: api/Absence/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAbsence(int id, Absence absence)
+        {
+            if (id != absence.AbsenceId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(absence).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AbsenceExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Absence
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Absence>> PostAbsence(Absence absence)
+        {
+            _context.Absences.Add(absence);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetAbsence", new { id = absence.AbsenceId }, absence);
+        }
+
+        // DELETE: api/Absence/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAbsence(int id)
+        {
+            var absence = await _context.Absences.FindAsync(id);
+            if (absence == null)
+            {
+                return NotFound();
+            }
+
+            _context.Absences.Remove(absence);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool AbsenceExists(int id)
+        {
+            return _context.Absences.Any(e => e.AbsenceId == id);
+        }
+    }
+}
