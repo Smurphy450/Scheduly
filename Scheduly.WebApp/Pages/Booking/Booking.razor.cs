f7b85ba6c295c46b11636f837fc4a5e5ee7848e5
Kevin Hornbek-Knudsen <kevinhornbek@gmail.com>
2024-11-15 13:23:53 +0100

    Booking page WIP

--- Scheduly.WebApp/Pages/Booking/Booking.razor.cs
+++ Scheduly.WebApp/Pages/Booking/Booking.razor.cs
@@ -1,8 +1,12 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Scheduly.WebApi.Models;
 
 namespace Scheduly.WebApp.Pages.Booking
 {
 	public class BookingBase : ComponentBase
 	{
+		public WebApi.Models.Booking bookingModel = new WebApi.Models.Booking();
+
+		GetBookings();
 	}
 }
