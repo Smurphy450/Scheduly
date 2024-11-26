cb7005679ef09d877afa1f109440d1e0bcad71c7
Kevin <kevinhornbek@gmail.com>
2024-11-26 13:34:08 +0100

    Added new table

---
+++ Scheduly.SqlDb/Schema/Tables/PremisCategory.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[PremisCategory]
+(
+	[PremisCategoryID] int not null identity(1,1),
+	[Name] nvarchar(50) not null,
+	constraint [PK_dbo_PremisCategory$PremisCategoryID] primary key clustered (PremisCategoryID) with (fillfactor = 100),
+)
