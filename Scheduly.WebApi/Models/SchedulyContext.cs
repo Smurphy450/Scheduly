9d44757648a97226170eb69f246ffb1f4bb472f4
Kevin Hornbek-Knudsen <kevinhornbek@gmail.com>
2024-11-27 10:03:40 +0100

    Added todo

--- Scheduly.WebApi/Models/SchedulyContext.cs
+++ Scheduly.WebApi/Models/SchedulyContext.cs
@@ -37,7 +37,9 @@ public partial class SchedulyContext : DbContext
 
     public virtual DbSet<ZipCode> ZipCodes { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+
+	// TODO: If API doesn't work, remove \\SQLEXPRESS
+	protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseSqlServer("Data Source=localhost\\SQLEXPRESS;Initial Catalog=Scheduly;TrustServerCertificate=True;Trusted_Connection=True;");
 
