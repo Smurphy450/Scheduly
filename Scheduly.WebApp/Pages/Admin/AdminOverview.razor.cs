0c543cf2b68f116af44ba31dfe12f76309efe056
WetDrywall <Teisvhjelm@gmail.com>
2024-11-29 18:38:29 +0100

    AdminOverview Glemt fra i går

---
+++ Scheduly.WebApp/Pages/Admin/AdminOverview.razor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Scheduly.WebApp.Pages.Admin
+{
+    public class AdminOverviewBase : ComponentBase
+    {
+        protected override void OnInitialized()
+        {
+            Console.WriteLine("Admin Overview Page Initialized.");
+        }
+    }
+}
