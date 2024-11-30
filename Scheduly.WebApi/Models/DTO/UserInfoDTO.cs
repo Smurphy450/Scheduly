bfba8047addf5f4c63cbfdc2fc6bab4f1457bbb6
WetDrywall <Teisvhjelm@gmail.com>
2024-11-30 15:23:08 +0100

    AdminOverview kan hente brugere

---
+++ Scheduly.WebApi/Models/DTO/UserInfoDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Scheduly.WebApi.Models.DTO
+{
+    public class UserInfoDTO
+    {
+        public int UserId { get; set; }
+        public string Username { get; set; }
+    }
+}
--- Scheduly.WebApp/Pages/Admin/AdminOverview.razor.cs
+++ Scheduly.WebApp/Pages/Admin/AdminOverview.razor.cs
@@ -1,12 +1,42 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Scheduly.WebApi.Models.DTO;
+using System.Net.Http.Json;
 
 namespace Scheduly.WebApp.Pages.Admin
 {
     public class AdminOverviewBase : ComponentBase
     {
-        protected override void OnInitialized()
+        public List<UserInfoDTO> UserList { get; set; } = new List<UserInfoDTO>();
+
+        protected override async Task OnInitializedAsync()
         {
-            Console.WriteLine("Admin Overview Page Initialized.");
+            await GetAllUsers();
+        }
+
+        private async Task GetAllUsers()
+        {
+            try
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    var response = await httpClient.GetAsync("https://localhost:7171/api/Users");
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var users = await response.Content.ReadFromJsonAsync<List<UserInfoDTO>>();
+                        UserList = users ?? new List<UserInfoDTO>();
+
+                        Console.WriteLine("Retrieved all users.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Failed to get all users. Status: {response.StatusCode}");
+                    }
+                }
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine($"An error occurred while making the request: {e.Message}");
+            }
         }
     }
 }
