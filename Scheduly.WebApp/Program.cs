36f7986554c2d52360334d0eb780fe387d412875
WetDrywall <Teisvhjelm@gmail.com>
2024-12-02 14:11:53 +0100

    Når de loader projectet første gang, så opretter den automatisk zipcodes og adminsettings

--- Scheduly.WebApp/Program.cs
+++ Scheduly.WebApp/Program.cs
@@ -1,3 +1,47 @@
+//using Microsoft.AspNetCore.Components;
+//using Microsoft.AspNetCore.Components.Web;
+//using MudBlazor;
+//using MudBlazor.Services;
+//using Scheduly.WebApi.Controllers;
+//using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+//using Microsoft.AspNetCore.Components.Authorization;
+//using Scheduly.WebApp.Authentication;
+
+//var builder = WebApplication.CreateBuilder(args);
+
+//// Add services to the container.
+//builder.Services.AddAuthenticationCore();
+//builder.Services.AddRazorPages();
+//builder.Services.AddServerSideBlazor();
+//builder.Services.AddScoped<ProtectedLocalStorage>();
+//builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
+//builder.Services.AddHttpClient<System.Net.Http.HttpClient>(options=>options.BaseAddress = new Uri("https://localhost:7171/"));
+//builder.Services.AddMudServices();
+
+//// Add MudBlazor services
+//builder.Services.AddMudServices();
+
+//var app = builder.Build();
+
+//// Configure the HTTP request pipeline.
+//if (!app.Environment.IsDevelopment())
+//{
+//	app.UseExceptionHandler("/Error");
+//	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+//	app.UseHsts();
+//}
+
+//app.UseHttpsRedirection();
+
+//app.UseStaticFiles();
+
+//app.UseRouting();
+
+//app.MapBlazorHub();
+//app.MapFallbackToPage("/_Host");
+
+//app.Run();
+
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using MudBlazor;
@@ -6,6 +50,8 @@ using Scheduly.WebApi.Controllers;
 using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
 using Microsoft.AspNetCore.Components.Authorization;
 using Scheduly.WebApp.Authentication;
+using Scheduly.WebApi.Models;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,20 +61,23 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddScoped<ProtectedLocalStorage>();
 builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
-builder.Services.AddHttpClient<System.Net.Http.HttpClient>(options=>options.BaseAddress = new Uri("https://localhost:7171/"));
+builder.Services.AddHttpClient<System.Net.Http.HttpClient>(options => options.BaseAddress = new Uri("https://localhost:7171/"));
 builder.Services.AddMudServices();
 
 // Add MudBlazor services
 builder.Services.AddMudServices();
+
+// Add DbContext
+builder.Services.AddDbContext<SchedulyContext>(options => options.UseSqlServer("name:ConnectionStrings:DefaultConnection"));
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-	app.UseExceptionHandler("/Error");
-	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-	app.UseHsts();
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
@@ -39,5 +88,14 @@ app.UseRouting();
 
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
+
+// Seed the database
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<SchedulyContext>();
+    context.SeedZipCodes();
+    context.SeedAdminSettings();
+}
 
 app.Run();
