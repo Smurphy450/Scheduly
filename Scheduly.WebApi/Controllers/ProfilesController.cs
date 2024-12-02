ed37fa12dd8034a73a307c79637dbcfcf8dfcb07
WetDrywall <Teisvhjelm@gmail.com>
2024-12-02 20:54:53 +0100

    ProfilesController ryddet op

--- Scheduly.WebApi/Controllers/ProfilesController.cs
+++ Scheduly.WebApi/Controllers/ProfilesController.cs
@@ -20,8 +20,7 @@ namespace Scheduly.WebApi.Controllers
         {
             _context = context;
         }
-        //change to [HttpGet("User/{userId}")]
-        // GET: api/ProfileDTO/User/{userId}
+        
         [HttpGet("UserDto/{userId}")] 
         public async Task<ActionResult<ProfileDTO>> GetProfileDTOByUserId(int userId)
         {
@@ -40,27 +39,6 @@ namespace Scheduly.WebApi.Controllers
                     Email = p.User.Email
                 })
                 .FirstOrDefaultAsync();
-
-            if (profile == null)
-            {
-                return NotFound();
-            }
-
-            return profile;
-        }
-
-        // GET: api/Profiles
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Profile>>> GetProfiles()
-        {
-            return await _context.Profiles.ToListAsync();
-        }
-
-        // GET: api/Profiles/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Profile>> GetProfile(int id)
-        {
-            var profile = await _context.Profiles.FindAsync(id);
 
             if (profile == null)
             {
@@ -207,49 +185,6 @@ namespace Scheduly.WebApi.Controllers
             return NoContent();
         }
 
-
-        // PUT: api/Profiles/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutProfile(int id, Profile profile)
-        {
-            if (id != profile.ProfileId)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(profile).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!ProfileExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
-        // POST: api/Profiles
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<Profile>> PostProfile(Profile profile)
-        {
-            _context.Profiles.Add(profile);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetProfile", new { id = profile.ProfileId }, profile);
-        }
-
         // POST: api/Profiles/UserProfile
         [HttpPost("UserProfile")]
         public async Task<ActionResult<UserProfileDTO>> PostUserProfile(UserProfileDTO userProfileDto)
@@ -296,22 +231,6 @@ namespace Scheduly.WebApi.Controllers
             await _context.SaveChangesAsync();
 
             return CreatedAtAction("GetUserProfileByUserId", new { userId = user.UserId }, userProfileDto);
-        }
-
-        // DELETE: api/Profiles/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteProfile(int id)
-        {
-            var profile = await _context.Profiles.FindAsync(id);
-            if (profile == null)
-            {
-                return NotFound();
-            }
-
-            _context.Profiles.Remove(profile);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
         }
 
         private bool ProfileExists(int id)
