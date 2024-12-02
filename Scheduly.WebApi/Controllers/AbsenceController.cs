2c033878ca3e9861fe91c68fb6c70ac89c18d803
WetDrywall <Teisvhjelm@gmail.com>
2024-12-02 20:43:13 +0100

    I gang med at rydde op i vores controllers.

--- Scheduly.WebApi/Controllers/AbsenceController.cs
+++ Scheduly.WebApi/Controllers/AbsenceController.cs
@@ -15,78 +15,13 @@ namespace Scheduly.WebApi.Controllers
         public AbsenceController(SchedulyContext context)
         {
             _context = context;
-        }
-
-        // GET: api/Absence
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Absence>>> GetAbsences()
-        {
-            return await _context.Absences.ToListAsync();
-        }
-
-        // GET: api/Absence/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Absence>> GetAbsence(int id)
-        {
-            var absence = await _context.Absences.FindAsync(id);
-
-            if (absence == null)
-            {
-                return NotFound();
-            }
-
-            return absence;
-        }
-
-        // GET: api/Absence/User/{userId}
-        [HttpGet("User/{userId}")]
-        public async Task<ActionResult<IEnumerable<Absence>>> GetAbsenceByUserId(int userId)
-        {
-            var absences = await _context.Absences
-                .Where(a => a.UserId == userId)
-                .ToListAsync();
-
-            if (absences.Count == 0)
-            {
-                return NotFound();
-            }
-
-            return absences;
-        }
-
-        [HttpGet("ApproveAbsence/{id}")]
-        public async Task<ActionResult<ApproveAbsenceDTO>> GetApproveAbsence(int id)
-        {
-            var absence = await _context.Absences
-                .Include(a => a.User)
-                .Include(a => a.AbsenceType)
-                .FirstOrDefaultAsync(a => a.AbsenceId == id);
-
-            if (absence == null)
-            {
-                return NotFound();
-            }
-
-            var approveAbsenceDTO = new ApproveAbsenceDTO
-            {
-                AbsenceId = absence.AbsenceId,
-                UserId = absence.UserId,
-                Username = absence.User.Username,
-                AbsenceTypeName = absence.AbsenceType.Name,
-                Start = absence.Start,
-                End = absence.End,
-                Description = absence.Description,
-                Approved = absence.Approved ?? false
-            };
-
-            return Ok(approveAbsenceDTO);
-        }
+        }        
 
         [HttpGet("PendingApproval")]
         public async Task<ActionResult<IEnumerable<ApproveAbsenceDTO>>> GetPendingApprovalAbsences()
         {
             var pendingAbsences = await _context.Absences
-                .Where(a => a.Approved == null && (a.End == null || a.End > DateTimeOffset.Now))
+                .Where(a => a.Approved == null && (a.End == null))
                 .Include(a => a.User)
                 .Include(a => a.AbsenceType)
                 .Select(a => new ApproveAbsenceDTO
@@ -128,74 +63,6 @@ namespace Scheduly.WebApi.Controllers
 
             return Ok(absences);
         }
-
-        // PUT: api/Absence/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutAbsence(int id, Absence absence)
-        {
-            if (id != absence.AbsenceId)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(absence).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!AbsenceExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
-        // POST: api/Absence
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<Absence>> PostAbsence(Absence absence)
-        {
-            _context.Absences.Add(absence);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetAbsence", new { id = absence.AbsenceId }, absence);
-        }
-
-        // POST: api/Absence/Report
-        //[HttpPost("Report")]
-        //public async Task<ActionResult<Absence>> ReportAbsence(ReportAbsenceDTO reportAbsenceDTO)
-        //{
-        //    var absenceType = await _context.AbsenceTypes.FindAsync(reportAbsenceDTO.AbsenceTypeId);
-        //    if (absenceType == null)
-        //    {
-        //        return BadRequest("Invalid AbsenceTypeId");
-        //    }
-
-        //    var absence = new Absence
-        //    {
-        //        AbsenceTypeId = reportAbsenceDTO.AbsenceTypeId,
-        //        UserId = reportAbsenceDTO.UserId,
-        //        Start = reportAbsenceDTO.DatetimeStart,
-        //        End = reportAbsenceDTO.DatetimeEnd,
-        //        Description = reportAbsenceDTO.Description,
-        //        Approved = !(absenceType.MustBeApproved ?? false)
-        //    };
-
-        //    _context.Absences.Add(absence);
-        //    await _context.SaveChangesAsync();
-
-        //    return CreatedAtAction("GetAbsence", new { id = absence.AbsenceId }, absence);
-        //}
 
         [HttpPost("Report")]
         public async Task<IActionResult> ReportAbsence(ReportAbsenceDTO reportAbsenceDTO)
--- Scheduly.WebApi/Controllers/AbsenceTypesController.cs
+++ Scheduly.WebApi/Controllers/AbsenceTypesController.cs
@@ -28,47 +28,6 @@ namespace Scheduly.WebApi.Controllers
             return await _context.AbsenceTypes.ToListAsync();
         }
 
-        // GET: api/AbsenceTypes/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<AbsenceType>> GetAbsenceType(int id)
-        {
-            var absenceType = await _context.AbsenceTypes.FindAsync(id);
-
-            if (absenceType == null)
-            {
-                return NotFound();
-            }
-
-            return absenceType;
-        }
-
-        [HttpGet("Names")]
-        public async Task<ActionResult<IEnumerable<string>>> GetAbsenceTypeNames()
-        {
-            var absenceTypeNames = await _context.AbsenceTypes
-                .Select(at => at.Name)
-                .ToListAsync();
-
-            return Ok(absenceTypeNames);
-        }
-
-        [HttpPost("{id}")]
-        public async Task<ActionResult<AbsenceType>> PostAbsenceType(int id, AbsenceTypeDTO absenceTypeDTO)
-        {
-            if (id == 0)
-            {
-                return Ok(new AbsenceTypeDTO());
-            }
-
-            var absenceType = await _context.AbsenceTypes.FindAsync(id);
-            if (absenceType == null)
-            {
-                return NotFound();
-            }
-
-            return Ok(absenceType);
-        }
-
         // POST: api/AbsenceTypes/Upsert
         [HttpPost("Upsert")]
         public async Task<IActionResult> UpsertAbsenceType(AbsenceTypeDTO absenceTypeDTO)
@@ -117,29 +76,16 @@ namespace Scheduly.WebApi.Controllers
 
                 return NoContent();
             }
-        }
+        }     
 
         //[HttpGet("UserAbsences/{userId}")]
-        //public async Task<ActionResult<IEnumerable<object>>> GetUserAbsenceTypes(int userId)
+        //public async Task<ActionResult<IEnumerable<UserAbsenceTypeDto>>> GetUserAbsenceTypes(int userId)
         //{
         //    var userAbsenceTypes = await _context.AbsenceTypes
-        //        .Select(at => new
+        //        .Select(at => new UserAbsenceTypeDto
         //        {
-        //            AbsenceType = at,
-        //            AbsenceCount = at.Absences.Count(a => a.UserId == userId)
-        //        })
-        //        .ToListAsync();
-
-        //    return Ok(userAbsenceTypes);
-        //}
-
-        //[HttpGet("UserAbsences/{userId}")]
-        //public async Task<ActionResult<IEnumerable<object>>> GetUserAbsenceTypes(int userId)
-        //{
-        //    var userAbsenceTypes = await _context.AbsenceTypes
-        //        .Select(at => new
-        //        {
-        //            AbsenceType = at,
+        //            AbsenceTypeId = at.AbsenceTypeId,
+        //            AbsenceTypeName = at.Name,
         //            AbsenceCount = at.Absences.Count(a => a.UserId == userId),
         //            TotalMinutes = at.Absences
         //                .Where(a => a.UserId == userId && a.End.HasValue)
@@ -149,67 +95,6 @@ namespace Scheduly.WebApi.Controllers
 
         //    return Ok(userAbsenceTypes);
         //}
-
-        [HttpGet("UserAbsences/{userId}")]
-        public async Task<ActionResult<IEnumerable<UserAbsenceTypeDto>>> GetUserAbsenceTypes(int userId)
-        {
-            var userAbsenceTypes = await _context.AbsenceTypes
-                .Select(at => new UserAbsenceTypeDto
-                {
-                    AbsenceTypeId = at.AbsenceTypeId,
-                    AbsenceTypeName = at.Name,
-                    AbsenceCount = at.Absences.Count(a => a.UserId == userId),
-                    TotalMinutes = at.Absences
-                        .Where(a => a.UserId == userId && a.End.HasValue)
-                        .Sum(a => EF.Functions.DateDiffMinute(a.Start, a.End.Value))
-                })
-                .ToListAsync();
-
-            return Ok(userAbsenceTypes);
-        }
-
-
-        // PUT: api/AbsenceTypes/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutAbsenceType(int id, AbsenceType absenceType)
-        {
-            if (id != absenceType.AbsenceTypeId)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(absenceType).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!AbsenceTypeExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
-        // POST: api/AbsenceTypes
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<AbsenceType>> PostAbsenceType(AbsenceType absenceType)
-        {
-            _context.AbsenceTypes.Add(absenceType);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetAbsenceType", new { id = absenceType.AbsenceTypeId }, absenceType);
-        }
 
         // DELETE: api/AbsenceTypes/5
         [HttpDelete("{id}")]
